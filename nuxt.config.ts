ca279bebb93e0372039061cb3f0bb3ef7081281f
alexandrupero <alexandrupero@users.noreply.github.com>
2023-07-10 22:09:47 +0100

    feat: add docsearch test integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: "@nuxt-themes/docus",
 
   modules: [
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
+    "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
-})
+    "@nuxt/devtools",
+  ],
+  runtimeConfig: {
+    public: {
+      algolia: {
+        apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
+        applicationId: process.env.ALGOLIA_APPLICATION_ID,
+        docSearch: {
+          indexName: process.env.ALGOLIA_INDEX_NAME,
+        },
+      },
+    },
+  },
+});
